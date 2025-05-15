ed2c7bdf076ae63d8528af63804c3e1eb68c2909
Pauline Moraa <pmoraa431@gmail.com>
2025-05-15 04:09:15 +0300

    Read text from a file. Save changes to a file.

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+from pathlib import Path
+
 def write_file(file_name, file_content):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, mode='a', encoding='utf-8') as file:
+        file.write(append_content)  # No newline added here
 
 def read_file(file_name):
-    pass
+    file_path = Path(file_name).with_suffix('.txt')
+    with open(file_path, mode='r', encoding='utf-8') as file:
+        return file.read()
